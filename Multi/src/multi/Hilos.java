eab8f4730af2376a390b307427878714053ba527
Nesemasiu <112734726+Nesemasiu@users.noreply.github.com>
2022-11-11 18:11:07 -0500

    Add files via upload

---
+++ Multi/src/multi/Hilos.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multi;
+
+/**
+ *
+ * @author Nesema
+ */
+/* Clase que define las características de un objeto
+
+* que denominaremos Hilo e imprime el nombre y tiempo de espera
+
+ */
+public class Hilos extends Thread {
+
+    private String nombre;
+
+    private int retardo;
+
+// Constructor para inicializar el nombre del hilo y tiempo de retardo
+    public Hilos(String s, int d) {
+
+        nombre = s;
+
+        retardo = d;
+
+    }
+
+// El metodo run() ejecuta
+// threads. Cuando run() termina el thread muere
+    public void run() {
+
+// Con el método sleep retrasamos la ejecución el tiempo especificado
+        try {
+
+            sleep(retardo);
+
+        } catch (InterruptedException e) {
+
+        }
+
+// Imprimimos el nombre y tiempo que hemos retardado el hilo
+        System.out.println("Prueba Hilos: " + nombre + " " + retardo);
+
+    }
+
+}
